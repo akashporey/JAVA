1def3b2ceaa20dc30b447fce57c499eae5fadbb8
Akash Porey <akashporeyofficial2507@gmail.com>
2025-11-28 03:10:56 +0530

    Java First Code

---
+++ FirstCode/FirstCode.java
@@ -0,0 +1,7 @@
+public class FirstCode {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Akash Porey.");
+        System.out.println("This is my first Java code.");
+    }
+}
