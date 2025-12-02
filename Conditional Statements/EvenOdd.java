23dbe8b03df0f944152ad1584bf38fcf397fbf92
Akash Porey <akashporeyofficial2507@gmail.com>
2025-12-02 23:59:28 +0530

    Add

---
+++ Conditional Statements/EvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class conditions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int age = sc.nextInt();
+
+        if (x%2==0) {
+            System.out.println("Even num");
+        }
+        else{
+            System.out.println("Not Adult");
+        }
+        sc.close();
+    }
+}
