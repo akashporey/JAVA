f385bb2904e96f6a7a71c5a05da15edd0965ab46
Akash Porey <akashporeyofficial2507@gmail.com>
2025-12-11 23:09:58 +0530

    Add File

---
+++ Loops in Java/loops2.java
@@ -0,0 +1,8 @@
+
+public class loops2 {
+    public static void main(String[] args) {
+        for(int counter =0; counter<11; counter++) {
+            System.out.println(counter);
+        }
+    }
+}
