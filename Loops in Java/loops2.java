38335eab1be910640dab4588dd8cc21a43a6865a
Akash Porey <akashporeyofficial2507@gmail.com>
2025-12-16 23:32:20 +0530

    Modified Code

--- Loops in Java/loops2.java
+++ Loops in Java/loops2.java
@@ -1,8 +1,12 @@
 
 public class loops2 {
     public static void main(String[] args) {
-        for(int counter =0; counter<11; counter++) {
-            System.out.println(counter);
+        // for(int counter =0; counter<11; counter++) {
+        //     System.out.println(counter);
+        // }
+
+        for(int i =0; i<11; i++) {
+            System.out.println(i);
         }
     }
 }
