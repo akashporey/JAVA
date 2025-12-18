0b5e51aeb842bc90798d11a4a630503daa0e9afd
Akash Porey <akashporeyofficial2507@gmail.com>
2025-12-18 23:03:22 +0530

    Modified Code

--- Loops in Java/forloop3.java
+++ Loops in Java/forloop3.java
@@ -1,11 +1,16 @@
+import java.util.*;
 
 public class forloop3 {
     public static void main(String[] args) {
-       int i=0;
-       while (i<11) {
-        System.out.println(i);
-        i++;
-       }
-        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum=0;
+
+        for(int i=0; i<=n; i++){
+            sum= sum+i;
+        }
+        System.out.println(sum);
+        sc.close();
     }
 }
