71b337dace3fe6b326ea24161494b27824ac43ec
Akash Porey <akashporeyofficial2507@gmail.com>
2025-12-18 22:45:04 +0530

    Add File

--- Loops in Java/whileloop2.java
+++ Loops in Java/whileloop2.java
@@ -1,3 +1,10 @@
-public class loops4 {
-    
+public class whileloop2 {
+    public static void main(String[] args) {
+        int i=0;
+
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<11);
+    }
 }
