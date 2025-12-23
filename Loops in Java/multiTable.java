0cc2955ba815fb9faef6776521dbce852d3008de
Akash Porey <akashporeyofficial2507@gmail.com>
2025-12-23 23:55:30 +0530

    Add File

--- Loops in Java/multiTable.java
+++ Loops in Java/multiTable.java
@@ -1,5 +1,15 @@
+import java.util.*;
+
 public class multiTable {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int r=0;
+        int n = sc.nextInt();
+        for(int i=1; i<11; i++){
+            r =n*i;
+            System.out.println(r);
+            //System.out.println(n+ "X" + i + "=" + n);
+        }
+        sc.close();
     }
 }
