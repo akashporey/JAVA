c04a9a42d9fd729ce0f32ba902f879d65312e837
Akash Porey <akashporeyofficial2507@gmail.com>
2025-12-24 23:34:31 +0530

    Add File

--- Loops in Java/pattern1.java
+++ Loops in Java/pattern1.java
@@ -1,3 +1,17 @@
-public class forloop4 {
-    
+import java.util.*;
+
+public class pattern1 {
+    public static void main(String[] args) {
+
+        int n=4, m=5;
+       
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
+
+
