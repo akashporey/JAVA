8e18ba365050504be4bf4595b79e9c22a6ad2239
Akash Porey <akashporeyofficial2507@gmail.com>
2025-12-26 23:30:15 +0530

    Modified Code

--- Loops in Java/pattern3.java
+++ Loops in Java/pattern3.java
@@ -2,20 +2,18 @@
 public class pattern3 {
     public static void main(String[] args) {
 
-        int n=4, m=5;
+        int n=8;
        
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if (i==1 || j==1 || i==n || j==m) {
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
-                }
-                
+            for(int j=1; j<=i; j++){
+        
+                System.out.print("*");
+                   
             }
             System.out.println();
         }
     }
 }
+
 
 
